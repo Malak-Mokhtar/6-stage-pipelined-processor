4f9293f7912182fc3076c9ddd276885ee4dad333
helloworld877 <mmghaly3@gmail.com>
2023-04-11 20:48:18 +0200

    fixed data memory to avoid latch

--- Data_Registers/Data_Memory.vhd
+++ Data_Registers/Data_Memory.vhd
@@ -22,15 +22,15 @@ BEGIN
         --async reset
         IF Rst = '1' THEN
             memory_data <= (OTHERS => (OTHERS => '0'));
-        END IF;
-        -- write on rising edge
-        IF rising_edge(clk) AND MemWrite_en = '1' THEN
-            memory_data(to_integer(unsigned(Address_Write_Data))) <= Write_Data;
-        END IF;
-        --read on falling edge
-        IF falling_edge(clk) AND MemRead_en = '1' THEN
 
-            Read_Data <= memory_data(to_integer(unsigned(Address_Read_Data)));
+        ELSIF rising_edge(clk) THEN
+            IF MemWrite_en = '1' THEN --write data
+                memory_data(to_integer(unsigned(Address_Write_Data))) <= Write_Data;
+                Read_Data <= (OTHERS => 'X');--read data is don't care
+            ELSIF MemRead_en = '1' THEN --read data
+                Read_Data <= memory_data(to_integer(unsigned(Address_Read_Data)));
+
+            END IF;
         END IF;
     END PROCESS; -- main_loop
 
