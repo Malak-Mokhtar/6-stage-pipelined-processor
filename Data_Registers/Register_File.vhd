802e82e5e7ec9a6792e43506fb7f67434b4cc103
helloworld877 <mmghaly3@gmail.com>
2023-04-11 21:24:45 +0200

    added register file

--- Data_Registers/Register_File.vhd
+++ Data_Registers/Register_File.vhd
@@ -4,7 +4,9 @@ USE ieee.numeric_std.ALL;
 
 ENTITY Reg_file IS
     PORT (
-        clock
+        clk, en : IN STD_LOGIC;
+        Read_Address1, Read_Address2, Write_Addesss : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+        Write_Data : IN STD_LOGIC_VECTOR(15 DOWNTO 0)
     );
 END Reg_file;
 
