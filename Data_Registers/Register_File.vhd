5de4d539f1ed8c2e8294d306799e8c46c82f0a24
helloworld877 <mmghaly3@gmail.com>
2023-04-11 20:55:14 +0200

    Create Register_File.vhd

---
+++ Data_Registers/Register_File.vhd
@@ -0,0 +1,15 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+ENTITY Reg_file IS
+    PORT (
+        clock
+    );
+END Reg_file;
+
+ARCHITECTURE arch OF Reg_file IS
+
+BEGIN
+
+END ARCHITECTURE;
