d8fb8012efb50d2724b3e495b1c599f3099ccad1
helloworld877 <mmghaly3@gmail.com>
2023-04-11 22:59:56 +0200

    finished register file

--- Data_Registers/Register_File.vhd
+++ Data_Registers/Register_File.vhd
@@ -4,7 +4,7 @@ USE ieee.numeric_std.ALL;
 
 ENTITY Reg_file IS
     PORT (
-        clk, Rst, en : IN STD_LOGIC;
+        clk, Rst, write_en : IN STD_LOGIC;
         Read_Address1, Read_Address2, Write_Addesss : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
         Read_Data1, Read_Data2 : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
         Write_Data : IN STD_LOGIC_VECTOR(15 DOWNTO 0)
@@ -23,7 +23,13 @@ BEGIN
     BEGIN
         IF Rst = '1' THEN
             register_data <= (OTHERS => (OTHERS => '0'));
-        ELSIF rising_edge(clk) AND en = '1' THEN
+        ELSIF rising_edge(clk) AND write_en = '1' THEN
+
+            register_data(to_integer(unsigned((Write_Addesss)))) <= Write_Data;
+
+        ELSIF falling_edge(clk) THEN
+            Read_Data1 <= register_data(to_integer(unsigned((Read_Address1))));
+            Read_Data2 <= register_data(to_integer(unsigned((Read_Address2))));
 
         END IF;
     END PROCESS; -- main_loop
