2391922af9e38048498516afafb2c8aba7d2ed4b
Malak-Mokhtar <malak.mokhtar@gmail.com>
2023-04-14 22:17:04 +0200

    Updated flag registers

--- flag_registers/NF.vhd
+++ flag_registers/NF.vhd
@@ -12,7 +12,7 @@ BEGIN
     BEGIN
         IF (rst = '1') THEN
             NF_OUT <= '0';
-        ELSIF clk'event AND clk = '1' AND DE_ALU_en_out = '1' THEN
+        ELSIF falling_edge(clk) AND DE_ALU_en_out = '1' THEN
             NF_OUT <= NF_ALU;
         END IF;
     END PROCESS;
