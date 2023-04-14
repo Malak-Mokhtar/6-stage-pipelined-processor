64480c08d44ff725608de5a2d8c9be5ffa30ab95
helloworld877 <mmghaly3@gmail.com>
2023-04-14 13:32:42 +0200

    updated register file for new inputs

--- Pipeline_Registers/FD_Register.vhd
+++ Pipeline_Registers/FD_Register.vhd
@@ -5,23 +5,29 @@ USE ieee.numeric_std.ALL;
 ENTITY FD_Register IS
     PORT (
         clk, en, rst : IN STD_LOGIC;
-        IN_data : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-        OUT_Data : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+        Inst : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        Read_Address, IN_PORT : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        FD_Inst_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        FD_Read_Address_out, FD_IN_PORT_out : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
     );
 END FD_Register;
 
 ARCHITECTURE arch OF FD_Register IS
 
-    SIGNAL data : STD_LOGIC_VECTOR(15 DOWNTO 0);
 BEGIN
     main_loop : PROCESS (clk, rst)
     BEGIN
 
         IF rst = '1' THEN --check on reset
-            data <= (OTHERS => '0');
+            --make all outputs zero
+            FD_Inst_out <= (OTHERS => '0');
+            FD_Read_Address_out <= (OTHERS => '0');
+            FD_IN_PORT_out <= (OTHERS => '0');
         ELSIF falling_edge(clk) AND en = '1' THEN --check on enable and falling edge
-            data <= IN_data;
+            FD_Inst_out <= Inst;
+            FD_Read_Address_out <= Read_Address;
+            FD_IN_PORT_out <= IN_PORT;
         END IF;
     END PROCESS; -- main_loop
-    OUT_Data <= data;
+
 END ARCHITECTURE;
