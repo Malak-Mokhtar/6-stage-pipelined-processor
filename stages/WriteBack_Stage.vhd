e2cc98c0178ed18e43cad0fa4140e69af60e031e
Makoish <mohamedmohsen96661@gmail.com>
2023-04-15 01:18:34 +0200

    Update WriteBack_Stage.vhd

--- stages/WriteBack_Stage.vhd
+++ stages/WriteBack_Stage.vhd
@@ -10,7 +10,7 @@ ENTITY WriteBack_Stage IS
         MW_IN_PORT_out,
         MW_ALU_Out_out,
         MW_Read_Data_out : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-        MW_Write_Addr_out  : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+        MW_Write_Addr_out  : OUT STD_LOGIC_VECTOR(2 DOWNTO 0)
     );
 END WriteBack_Stage;
 
@@ -26,9 +26,12 @@ ARCHITECTURE arch OF WriteBack_Stage IS
 BEGIN
 
     MUX_MAP : MUX_WB PORT MAP(
-        Read_Data,ALU_out,IN_PORT: IN std_logic_vector (15 DOWNTO 0);
-			Mem_to_Reg_en, IN_en : IN  std_logic;
-			Write_Data : OUT std_logic_vector (15 DOWNTO 0)
+       Read_Data => MW_Read_Data_out,
+       ALU_out=> MW_ALU_Out_out,
+       IN_PORT => MW_IN_PORT_out,
+       Mem_to_Reg_en=> open, 
+       IN_en => MW_IN_PORT_out,
+       Write_Data=> open
     );
 
 END ARCHITECTURE;
