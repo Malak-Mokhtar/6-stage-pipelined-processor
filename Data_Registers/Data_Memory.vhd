0f6b558e14a26e8feab24771aaaed316d5537b68
Malak-Mokhtar <malak.mokhtar@gmail.com>
2023-04-15 01:08:46 +0200

    Data memory updated

--- Data_Registers/Data_Memory.vhd
+++ Data_Registers/Data_Memory.vhd
@@ -16,30 +16,39 @@ ARCHITECTURE arch OF Data_Memory IS
     SIGNAL memory_data : Mem_type;
     --SIGNAL Address_sig1, Write_Data_sig1: STD_LOGIC_VECTOR(15 DOWNTO 0);
     --SIGNAL Rst_sig1, MemWrite_en_sig1, MemRead_en_sig1 : STD_LOGIC;
-    variable counter: integer := 0;
-    variable Address_var, Write_Data_var: STD_LOGIC_VECTOR(15 DOWNTO 0);
-    variable Rst_var, MemWrite_en_var, MemRead_en_var: STD_LOGIC_VECTOR;
 
 BEGIN
 
     main_loop : PROCESS (clk, Rst)
+
+    variable counter: integer := 0;
+    variable Address_var, Write_Data_var: STD_LOGIC_VECTOR(15 DOWNTO 0);
+    variable Rst_var, MemWrite_en_var, MemRead_en_var: STD_LOGIC := '0';
+
     BEGIN
         --async reset
-        IF Rst = '1' THEN
+        IF Rst_var = '1' THEN
             memory_data <= (OTHERS => (OTHERS => '0'));
 
         ELSIF falling_edge(clk) and counter = 2 THEN
-            IF MemWrite_en = '1' THEN --write data
-                memory_data(to_integer(unsigned(Address))) <= Write_Data;
+            IF MemWrite_en_var = '1' THEN --write data
+                memory_data(to_integer(unsigned(Address_var))) <= Write_Data_var;
                 Read_Data <= (OTHERS => '-');--read data is don't care
-            ELSIF MemRead_en = '1' THEN --read data
-                Read_Data <= memory_data(to_integer(unsigned(Address)));
+            ELSIF MemRead_en_var = '1' THEN --read data
+                Read_Data <= memory_data(to_integer(unsigned(Address_var)));
             ELSE
                 Read_Data <= (OTHERS => '-');--read data is don't care
             END IF;
-        counter := 0;
-        ELSIF falling_edge(clk) and counter /= 2 THEN
+            counter := 0;
+        ELSIF falling_edge(clk) and counter = 0 THEN
             counter := counter + 1;
+            Address_var:=Address;
+            Write_Data_var:=Write_Data;
+            Rst_var:=Rst;
+            MemWrite_en_var:=MemWrite_en;
+            MemRead_en_var:=MemRead_en;
+        ELSIF falling_edge(clk) and counter = 1 THEN
+        counter := counter + 1;
         END IF;
     END PROCESS; -- main_loop
 
