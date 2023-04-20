d5aa080a2e0b7e91bbe7e312509e7d3fbfc87282
helloworld877 <mmghaly3@gmail.com>
2023-04-20 03:28:46 +0200

    Update MM_Register.vhd

--- Pipeline_Registers/MM_Register.vhd
+++ Pipeline_Registers/MM_Register.vhd
@@ -6,14 +6,16 @@ ENTITY MM_Register IS
     PORT (
         clk, en, rst : IN STD_LOGIC;
         EM_IN_en_out : IN STD_LOGIC;
-	EM_IN_PORT_out, EM_ALU_Out_out: IN STD_LOGIC_vector (15 DOWNTO 0);
-	EM_Write_Addr_out: IN STD_LOGIC_vector (2 downto 0);
-	EM_RegWrite_en_out, EM_Mem_to_Reg_en_out: IN std_logic;
+        EM_IN_PORT_out, EM_ALU_Out_out : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+        EM_Write_Addr_out : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+        EM_RegWrite_en_out, EM_Mem_to_Reg_en_out : IN STD_LOGIC;
 
-	MM_IN_en_out: OUT std_logic;
-	MM_IN_PORT_out, MM_ALU_Out_out: OUT std_logic_vector (15 downto 0);
-	MM_Write_Addr_out: OUT std_logic_vector (2 downto 0);
-    MM_RegWrite_en_out, MM_Mem_to_Reg_en_out: OUT std_logic
+        MM_IN_en_out : OUT STD_LOGIC;
+        MM_IN_PORT_out, MM_ALU_Out_out : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
+        MM_Write_Addr_out : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
+        MM_RegWrite_en_out, MM_Mem_to_Reg_en_out : OUT STD_LOGIC;
+        EM_Memory_Reset_out : IN STD_LOGIC;
+        MM_Memory_Reset_out : OUT STD_LOGIC
     );
 END MM_Register;
 
@@ -24,20 +26,24 @@ BEGIN
     BEGIN
 
         IF rst = '1' THEN --check on reset
-        --make all outputs zero
-	    MM_IN_en_out <= '0';
-        MM_IN_PORT_out <= (OTHERS => '0');
-        MM_Write_Addr_out <= (OTHERS => '0');
-	    MM_ALU_Out_out <= (OTHERS => '0');
-	    MM_RegWrite_en_out <= '0';
-        MM_Mem_to_Reg_en_out <= '0';
+            --make all outputs zero
+            MM_IN_en_out <= '0';
+            MM_IN_PORT_out <= (OTHERS => '0');
+            MM_Write_Addr_out <= (OTHERS => '0');
+            MM_ALU_Out_out <= (OTHERS => '0');
+            MM_RegWrite_en_out <= '0';
+            MM_Mem_to_Reg_en_out <= '0';
         ELSIF falling_edge(clk) AND en = '1' THEN --check on enable and falling edge
-        MM_IN_en_out <= EM_IN_en_out;
-	    MM_IN_PORT_out <= EM_IN_PORT_out;
-	    MM_ALU_Out_out <= EM_ALU_Out_out;
-	    MM_Write_Addr_out <= EM_Write_Addr_out;
-	    MM_RegWrite_en_out <= EM_RegWrite_en_out;
-        MM_Mem_to_Reg_en_out <= EM_Mem_to_Reg_en_out;
+            MM_IN_en_out <= EM_IN_en_out;
+            MM_IN_PORT_out <= EM_IN_PORT_out;
+            MM_ALU_Out_out <= EM_ALU_Out_out;
+            MM_Write_Addr_out <= EM_Write_Addr_out;
+            MM_RegWrite_en_out <= EM_RegWrite_en_out;
+            MM_Mem_to_Reg_en_out <= EM_Mem_to_Reg_en_out;
+        END IF;
+        IF falling_edge(clk) THEN
+            MM_Memory_Reset_out <= EM_Memory_Reset_out;
+
         END IF;
     END PROCESS; -- main_loop
 
