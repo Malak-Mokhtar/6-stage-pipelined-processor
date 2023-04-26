9f357153c42e4f7d4505fd56edbbc3af460e3d69
helloworld877 <mmghaly3@gmail.com>
2023-04-26 20:20:33 +0200

    finished assembler v1

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -1,5 +1,15 @@
 
+Register_Translation = {"R0": "000",
+                        "R1": "001",
+                        "R2": "010",
+                        "R3": "011",
+                        "R4": "100",
+                        "R5": "101",
+                        "R6": "110",
+                        "R7": "111"}
 # function for loading file
+
+
 def load_file():
 
     f = open("TestcasesPhaseOne.asm", 'r', encoding='utf-8')
@@ -14,6 +24,358 @@ def load_file():
 
 
 def conversion(assembly_instructions: list[str]):
+    result = []
 
     for instruction in assembly_instructions:
+        # form bit string
+        temp_inst = ""
         instruction_line = instruction.split(" ")
+        if instruction_line[0] == "NOP":
+            # OPCODE
+            temp_inst += "00000"
+            # Rs1
+            temp_inst += "000"
+            # Rs2
+            temp_inst += "000"
+            # Rd
+            temp_inst += "000"
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("NOP")
+        elif instruction_line[0] == "SETC":
+            # OPCODE
+            temp_inst += "00001"
+            # Rs1
+            temp_inst += "000"
+            # Rs2
+            temp_inst += "000"
+            # Rd
+            temp_inst += "000"
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("SETC")
+        elif instruction_line[0] == "CLRC":
+            # OPCODE
+            temp_inst += "00010"
+            # Rs1
+            temp_inst += "000"
+            # Rs2
+            temp_inst += "000"
+            # Rd
+            temp_inst += "000"
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("CLRC")
+        elif instruction_line[0] == "OUT":
+            # OPCODE
+            temp_inst += "00011"
+            # Rs1 (Garbage)
+            temp_inst += "000"
+            # Rs2
+            temp_inst += Register_Translation[instruction_line[1]]
+            # Rd (Garbage)
+            temp_inst += "000"
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("OUT")
+        elif instruction_line[0] == "IN":
+            # OPCODE
+            temp_inst += "00100 "
+            # Rs1(Garbage)
+            temp_inst += "000"
+            # Rs2(Garbage)
+            temp_inst += "000"
+            # Rd
+            temp_inst += Register_Translation[instruction_line[1]]
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("IN")
+        elif instruction_line[0] == "MOV":
+            # getting the two registers
+            registers = instruction_line[1].split(",")
+            # OPCODE
+            temp_inst += "00101 "
+            # Rs1(Garbage)
+            temp_inst += "000"
+            # Rs2
+            temp_inst += Register_Translation[registers[1]]
+            # Rd
+            temp_inst += Register_Translation[registers[0]]
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("MOV")
+        elif instruction_line[0] == "IADD":
+            registers = instruction_line[1].split(",")
+            # OPCODE
+            temp_inst += "01000 "
+            # Rs1
+            temp_inst += Register_Translation[registers[1]]
+            # Rs2
+            temp_inst += "000"
+            # Rd
+            temp_inst += Register_Translation[registers[0]]
+            # Default Garbage
+            temp_inst += "00"
+            # add operation to results
+            result.append(temp_inst)
+            # to get immediate
+            temp_inst = bin(int("0x"+"F"+registers[2], 16))[6:]
+            # print
+            print("IADD")
+        elif instruction_line[0] == "ADD":
+            registers = instruction_line[1].split(",")
+            # OPCODE
+            temp_inst += "01001 "
+            # Rs1
+            temp_inst += Register_Translation[registers[1]]
+            # Rs2
+            temp_inst += Register_Translation[registers[2]]
+            # Rd
+            temp_inst += Register_Translation[registers[0]]
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("ADD")
+        elif instruction_line[0] == "SUB":
+            registers = instruction_line[1].split(",")
+            # OPCODE
+            temp_inst += "01010 "
+            # Rs1
+            temp_inst += Register_Translation[registers[1]]
+            # Rs2
+            temp_inst += Register_Translation[registers[2]]
+            # Rd
+            temp_inst += Register_Translation[registers[0]]
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("SUB")
+        elif instruction_line[0] == "AND":
+            registers = instruction_line[1].split(",")
+            # OPCODE
+            temp_inst += "01011 "
+            # Rs1
+            temp_inst += Register_Translation[registers[1]]
+            # Rs2
+            temp_inst += Register_Translation[registers[2]]
+            # Rd
+            temp_inst += Register_Translation[registers[0]]
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("AND")
+        elif instruction_line[0] == "OR":
+            registers = instruction_line[1].split(",")
+            # OPCODE
+            temp_inst += "01100 "
+            # Rs1
+            temp_inst += Register_Translation[registers[1]]
+            # Rs2
+            temp_inst += Register_Translation[registers[2]]
+            # Rd
+            temp_inst += Register_Translation[registers[0]]
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("OR")
+        elif instruction_line[0] == "NOT":
+            registers = instruction_line[1].split(",")
+            # OPCODE
+            temp_inst += "01101 "
+            # Rs1
+            temp_inst += Register_Translation[registers[1]]
+            # Rs2 (Garbage)
+            temp_inst += "000"
+            # Rd
+            temp_inst += Register_Translation[registers[0]]
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("NOT")
+        elif instruction_line[0] == "INC":
+            registers = instruction_line[1].split(",")
+            # OPCODE
+            temp_inst += "01110 "
+            # Rs1
+            temp_inst += Register_Translation[registers[1]]
+            # Rs2 (Garbage)
+            temp_inst += "000"
+            # Rd
+            temp_inst += Register_Translation[registers[0]]
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("INC")
+        elif instruction_line[0] == "DEC":
+            registers = instruction_line[1].split(",")
+            # OPCODE
+            temp_inst += "01110 "
+            # Rs1
+            temp_inst += Register_Translation[registers[1]]
+            # Rs2 (Garbage)
+            temp_inst += "000"
+            # Rd
+            temp_inst += Register_Translation[registers[0]]
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("DEC")
+        elif instruction_line[0] == "LDM":
+            registers = instruction_line[1].split(",")
+            # OPCODE
+            temp_inst += "10000 "
+            # Rs1 (Garbage)
+            temp_inst += "000"
+            # Rs2 (Garbage)
+            temp_inst += "000"
+            # Rd
+            temp_inst += Register_Translation[registers[0]]
+            # Default Garbage
+            temp_inst += "00"
+            # add instruction
+            result.append(temp_inst)
+            # to get immediate
+            temp_inst = bin(int("0x"+"F"+registers[1], 16))[6:]
+            # print
+            print("LDM")
+        elif instruction_line[0] == "PUSH":
+            # OPCODE
+            temp_inst += "10001 "
+            # Rs1 (Garbage)
+            temp_inst += "000"
+            # Rs2
+            temp_inst += Register_Translation[instruction_line[1]]
+            # Rd (Garbage)
+            temp_inst += "000"
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("PUSH")
+        elif instruction_line[0] == "POP":
+            # OPCODE
+            temp_inst += "10010 "
+            # Rs1 (Garbage)
+            temp_inst += "000"
+            # Rs2 (Garbage)
+            temp_inst += "000"
+            # Rd
+            temp_inst += Register_Translation[instruction_line[1]]
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("POP")
+        elif instruction_line[0] == "LDD":
+            registers = instruction_line[1].split(",")
+            # OPCODE
+            temp_inst += "10011 "
+            # Rs1 (Garbage)
+            temp_inst += "000"
+            # Rs2
+            temp_inst += Register_Translation[registers[1]]
+            # Rd
+            temp_inst += Register_Translation[registers[0]]
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("LDD")
+        elif instruction_line[0] == "STD":
+            registers = instruction_line[1].split(",")
+            # OPCODE
+            temp_inst += "10100 "
+            # Rs1
+            temp_inst += Register_Translation[registers[0]]
+            # Rs2
+            temp_inst += Register_Translation[registers[1]]
+            # Rd (Garbage)
+            temp_inst += "000"
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("STD")
+        elif instruction_line[0] == "JZ":
+            # OPCODE
+            temp_inst += "11000 "
+            # Rs1
+            temp_inst += Register_Translation[instruction_line[1]]
+            # Rs2 (Garbage)
+            temp_inst += "000"
+            # Rd (Garbage)
+            temp_inst += "000"
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("JZ")
+        elif instruction_line[0] == "JC":
+            # OPCODE
+            temp_inst += "11000 "
+            # Rs1
+            temp_inst += Register_Translation[instruction_line[1]]
+            # Rs2 (Garbage)
+            temp_inst += "000"
+            # Rd (Garbage)
+            temp_inst += "000"
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("JC")
+        elif instruction_line[0] == "JMP":
+            # OPCODE
+            temp_inst += "11010 "
+            # Rs1
+            temp_inst += Register_Translation[instruction_line[1]]
+            # Rs2
+            temp_inst += "000"
+            # Rd
+            temp_inst += "000"
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("JMP")
+        elif instruction_line[0] == "CALL":
+            # OPCODE
+            temp_inst += "11011 "
+            # Rs1
+            temp_inst += Register_Translation[instruction_line[1]]
+            # Rs2 (Garbage)
+            temp_inst += "000"
+            # Rd (Garbage)
+            temp_inst += "000"
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("CALL")
+        elif instruction_line[0] == "RET":
+            # OPCODE
+            temp_inst += "11100 "
+            # Rs1 (Garbage)
+            temp_inst += "000"
+            # Rs2 (Garbage)
+            temp_inst += "000"
+            # Rd (Garbage)
+            temp_inst += "000"
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("RET")
+        elif instruction_line[0] == "RTI":
+            # OPCODE
+            temp_inst += "11101 "
+            # Rs1 (Garbage)
+            temp_inst += "000"
+            # Rs2 (Garbage)
+            temp_inst += "000"
+            # Rd (Garbage)
+            temp_inst += "000"
+            # Default Garbage
+            temp_inst += "00"
+            # print
+            print("RTI")
+        # add inst to result array
+        result.append(temp_inst)
+    print(result)
