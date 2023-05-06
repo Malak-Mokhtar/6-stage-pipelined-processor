cb66aee0b184cf84c22ab8377ba07994a48018e1
helloworld877 <mmghaly3@gmail.com>
2023-05-07 01:16:00 +0300

    Update Execute_Stage.vhd

--- stages/Execute_Stage.vhd
+++ stages/Execute_Stage.vhd
@@ -25,32 +25,26 @@ ENTITY Execute_Stage IS
         DE_SP_inc_en_out : IN STD_LOGIC;
         DE_Read_Data1_out,
         DE_Read_Data2_out, MW_Read_Data_out,
-        MM_ALU_OUT, EM_ALU_OUT, Write_data :IN STD_LOGIC_VECTOR(15 DOWNTO 0); 
-        DE_Read_Address1, 
-        DE_Read_Address2, EM_Write_Addr_out, MM_Write_Addr_out, MW_Write_Addr_out: IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+        MM_ALU_OUT, EM_ALU_OUT, Write_data : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        DE_Read_Address1,
+        DE_Read_Address2, EM_Write_Addr_out, MM_Write_Addr_out, MW_Write_Addr_out : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
         DE_OPCODE_out : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
-
-
-       
-
 
         -- OUTPUT PORTS
         en_structural,
         ZF_OUT,
         CF_OUT,
         NF_OUT,
-            
+
         DE_Carry_en,
         DE_MemWrite_en,
         DE_MemRead_en,
-        DE_RTI_en_out: OUT STD_LOGIC;   
+        DE_RTI_en_out : OUT STD_LOGIC;
         ALU_Out,
         DE_Read_Data1_final,
         DE_Read_Data2_final,
         SP_before,
         SP_after : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
-    
-
 
     );
 END Execute_Stage;
@@ -69,177 +63,147 @@ ARCHITECTURE arch OF Execute_Stage IS
 
     --flags--
 
-    component Flag_Restoring IS 
-	PORT ( Read_Data: IN std_logic_vector (15 DOWNTO 0);
-			ZF_RTI,CF_RTI,NF_RTI : OUT std_logic);
-    END component;
+    COMPONENT Flag_Restoring IS
+        PORT (
+            Read_Data : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+            ZF_RTI, CF_RTI, NF_RTI : OUT STD_LOGIC);
+    END COMPONENT;
 
-    component Zero_except_RTI IS 
-	PORT ( ZF_ALU,JZ_en : IN  std_logic;
-			ZF_EXCEPT_RTI : OUT std_logic);
-    END component;
+    COMPONENT Zero_except_RTI IS
+        PORT (
+            ZF_ALU, JZ_en : IN STD_LOGIC;
+            ZF_EXCEPT_RTI : OUT STD_LOGIC);
+    END COMPONENT;
 
-    component Carry_except_RTI IS 
-	PORT ( CF_ALU,SETC_en,CLRC_en,JC_en : IN  std_logic;
-			CF_EXCEPT_RTI : OUT std_logic);
-    END component;
+    COMPONENT Carry_except_RTI IS
+        PORT (
+            CF_ALU, SETC_en, CLRC_en, JC_en : IN STD_LOGIC;
+            CF_EXCEPT_RTI : OUT STD_LOGIC);
+    END COMPONENT;
+    COMPONENT MUX_ZF IS
+        PORT (
+            ZF_EXCEPT_RTI, ZF_RTI, FLAGS_en : IN STD_LOGIC;
+            ZF_selected : OUT STD_LOGIC);
+    END COMPONENT;
 
+    COMPONENT MUX_CF IS
+        PORT (
+            CF_EXCEPT_RTI, CF_RTI, FLAGS_en : IN STD_LOGIC;
+            CF_selected : OUT STD_LOGIC);
+    END COMPONENT;
+    COMPONENT MUX_NF IS
+        PORT (
+            NF_ALU, NF_RTI, FLAGS_en : IN STD_LOGIC;
+            NF_selected : OUT STD_LOGIC);
+    END COMPONENT;
+    COMPONENT ZF IS
+        PORT (
+            ZF_Selected, clk, rst, DE_ALU_en_out, MW_FLAGS_en_out, DE_JZ_en_out : IN STD_LOGIC;
+            ZF_OUT : OUT STD_LOGIC);
+    END COMPONENT;
 
-    component MUX_ZF IS 
-	PORT ( ZF_EXCEPT_RTI,ZF_RTI,FLAGS_en : IN  std_logic;
-			ZF_selected : OUT std_logic);
-    END component;
+    COMPONENT CF IS
+        PORT (
+            CF_Selected, clk, rst, DE_Carry_en_out, MW_FLAGS_en_out : IN STD_LOGIC;
+            CF_OUT : OUT STD_LOGIC);
+    END COMPONENT;
+    COMPONENT NF IS
+        PORT (
+            NF_Selected, clk, rst, DE_ALU_en_out, MW_FLAGS_en_out : IN STD_LOGIC;
+            NF_OUT : OUT STD_LOGIC);
+    END COMPONENT;
+    COMPONENT Data_forwarding IS
+        PORT (
+            DE_Read_Address1, DE_Read_Address2, EM_Write_Addr_out, MM_Write_Addr_out, MW_Write_Addr_out : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+            EM_RegWrite_en_out, MM_RegWrite_en_out, MW_RegWrite_en_out : IN STD_LOGIC;
+            Read_data2_sel, Read_data1_sel : OUT STD_LOGIC_VECTOR(1 DOWNTO 0));
+    END COMPONENT;
 
-    component MUX_CF IS 
-	PORT ( CF_EXCEPT_RTI,CF_RTI,FLAGS_en : IN  std_logic;
-			CF_selected : OUT std_logic);
-    END component;
+    COMPONENT Structural_HDU IS
+        PORT (
+            DE_MemRead_en_out, EM_MemRead_en_out, DE_MemWrite_en_out, EM_MemWrite_en_out : IN STD_LOGIC;
+            en_structural : OUT STD_LOGIC);
+    END COMPONENT;
+    COMPONENT MUX_ALU_OP1 IS
+        PORT (
+            DE_Read_Data1_out, MM_ALU_Out_out, EM_ALU_out, Write_Data : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+            DE_Read_Data1_final_out : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
+            Read_data1_sel : IN STD_LOGIC_VECTOR(1 DOWNTO 0));
 
+    END COMPONENT;
+    COMPONENT MUX_ALU_OP2 IS
+        PORT (
+            DE_Read_Data2_out, MM_ALU_Out_out, EM_ALU_out, Write_Data : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+            DE_Read_Data2_final_out : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
+            Read_data2_sel : IN STD_LOGIC_VECTOR(1 DOWNTO 0));
 
-    component MUX_NF IS 
-	PORT ( NF_ALU,NF_RTI,FLAGS_en : IN  std_logic;
-			NF_selected : OUT std_logic);
-    END component;
+    END COMPONENT;
 
-
-    component ZF IS
-    PORT (
-        ZF_Selected, clk, rst, DE_ALU_en_out, MW_FLAGS_en_out, DE_JZ_en_out : IN STD_LOGIC;
-        ZF_OUT : OUT STD_LOGIC);
-    END component;
-
-    component CF IS
-    PORT (
-        CF_Selected, clk, rst, DE_Carry_en_out, MW_FLAGS_en_out : IN STD_LOGIC;
-        CF_OUT : OUT STD_LOGIC);
-    END component;
-
-
-    component NF IS
-    PORT (
-        NF_Selected, clk, rst, DE_ALU_en_out, MW_FLAGS_en_out : IN STD_LOGIC;
-        NF_OUT : OUT STD_LOGIC);
-    end component;
-
-
-    component Data_forwarding is
-
-
-    PORT(DE_Read_Address1, DE_Read_Address2, EM_Write_Addr_out, MM_Write_Addr_out, MW_Write_Addr_out: IN std_logic_vector(2 downto 0);
-    EM_RegWrite_en_out, MM_RegWrite_en_out, MW_RegWrite_en_out: IN std_logic;
-    Read_data2_sel, Read_data1_sel: out std_logic_vector(1 downto 0));
-    
-    
-    end component;
-
-    component Structural_HDU IS 
-	PORT ( DE_MemRead_en_out,EM_MemRead_en_out,DE_MemWrite_en_out,EM_MemWrite_en_out : IN  std_logic;
-			en_structural : OUT std_logic);
-    END component;
-
-
-    component MUX_ALU_OP1 is
-
-
-        PORT(DE_Read_Data1_out, MM_ALU_Out_out, EM_ALU_out, Write_Data: IN std_logic_vector(15 downto 0);
-        DE_Read_Data1_final_out: OUT std_logic_vector (15 downto 0);
-        Read_data1_sel: in std_logic_vector(1 downto 0));
-        
-    end component;
-
-
-    component MUX_ALU_OP2 is
-
-
-        PORT(DE_Read_Data2_out, MM_ALU_Out_out, EM_ALU_out, Write_Data: IN std_logic_vector(15 downto 0);
-        DE_Read_Data2_final_out: OUT std_logic_vector (15 downto 0);
-        Read_data2_sel: in std_logic_vector(1 downto 0));
-        
-    end component;
-
-
-
-    component Stack_Pointer IS
-    PORT (
-        clk, rst, en : IN STD_LOGIC;
-        data : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-        out_data : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
-    );
-    END component;
-
-
-    component SP_ALU IS
-    PORT (
-        SP_en, inc_en : IN STD_LOGIC;
-        sp_before : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-        sp_after : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
-    );
-    END component;
-
-
-    
-
+    COMPONENT Stack_Pointer IS
+        PORT (
+            clk, rst, en : IN STD_LOGIC;
+            data : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+            out_data : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+        );
+    END COMPONENT;
+    COMPONENT SP_ALU IS
+        PORT (
+            SP_en, inc_en : IN STD_LOGIC;
+            sp_before : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+            sp_after : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+        );
+    END COMPONENT;
     -------------------SIGNALS----------------
     SIGNAL ZF_SIG, NF_SIG, CF_SIG, ZF_EXCEPT_RTI_SIG, CF_EXCEPT_RTI_SIG, ZF_RTI_SIG, CF_RTI_SIG, NF_RTI_SIG,
     ZF_selected_SIG, CF_selected_SIG, NF_selected_SIG : STD_LOGIC;
 
-    SIGNAL DE_Read_Data1_final_out_sig, DE_Read_Data2_final_out_sig, SP_before_sig, SP_after_sig : std_logic_vector (15 downto 0);
+    SIGNAL DE_Read_Data1_final_out_sig, DE_Read_Data2_final_out_sig, SP_before_sig, SP_after_sig : STD_LOGIC_VECTOR (15 DOWNTO 0);
 
-    SIGNAL Read_data1_sel_sig, Read_data2_sel_sig: std_logic_vector (1 downto 0);
-
-
+    SIGNAL Read_data1_sel_sig, Read_data2_sel_sig : STD_LOGIC_VECTOR (1 DOWNTO 0);
 
 BEGIN
-
-
-
-
-    Flag_Restoring_MAP: Flag_Restoring PORT MAP(
+    Flag_Restoring_MAP : Flag_Restoring PORT MAP(
         Read_Data => MW_Read_Data_out,
         ZF_RTI => ZF_RTI_SIG,
         CF_RTI => CF_RTI_SIG,
         NF_RTI => NF_RTI_SIG
     );
 
-
-
-    Zero_except_RTI_MAP: Zero_except_RTI PORT MAP (
+    Zero_except_RTI_MAP : Zero_except_RTI PORT MAP(
         ZF_ALU => ZF_SIG,
         JZ_en => DE_JZ_en_out,
         ZF_EXCEPT_RTI => ZF_EXCEPT_RTI_SIG
     );
 
-    Carry_except_RTI_MAP: Carry_except_RTI PORT MAP (
+    Carry_except_RTI_MAP : Carry_except_RTI PORT MAP(
         CF_ALU => CF_SIG,
         SETC_en => DE_SETC_en_out,
         CLRC_en => DE_CLRC_en_out,
         JC_en => DE_JC_en_out,
         CF_EXCEPT_RTI => CF_EXCEPT_RTI_SIG
     );
-
-
-    MUX_ZF_MAP: MUX_ZF port map (
+    MUX_ZF_MAP : MUX_ZF PORT MAP(
         ZF_EXCEPT_RTI => ZF_EXCEPT_RTI_SIG,
         ZF_RTI => ZF_RTI_SIG,
         FLAGS_en => MW_FLAGS_en_out,
         ZF_selected => ZF_selected_SIG
     );
 
-    MUX_CF_MAP: MUX_CF port map(
+    MUX_CF_MAP : MUX_CF PORT MAP(
         CF_EXCEPT_RTI => CF_EXCEPT_RTI_SIG,
         CF_RTI => CF_RTI_SIG,
         FLAGS_en => MW_FLAGS_en_out,
         CF_selected => CF_selected_SIG
     );
 
-    MUX_NF_MAP: MUX_NF port map(
+    MUX_NF_MAP : MUX_NF PORT MAP(
         NF_ALU => NF_SIG,
         NF_RTI => NF_RTI_SIG,
         FLAGS_en => MW_FLAGS_en_out,
         NF_selected => NF_selected_SIG
     );
-    
-    ZF_MAP: ZF port map(
+
+    ZF_MAP : ZF PORT MAP(
         ZF_Selected => ZF_selected_SIG,
         DE_JZ_en_out => DE_JZ_en_out,
         MW_FLAGS_en_out => MW_FLAGS_en_out,
@@ -249,7 +213,7 @@ BEGIN
         rst => general_rst
     );
 
-    NF_MAP: NF port map(
+    NF_MAP : NF PORT MAP(
         NF_Selected => NF_selected_SIG,
         DE_ALU_en_out => DE_ALU_en_out,
         MW_FLAGS_en_out => MW_FLAGS_en_out,
@@ -258,17 +222,15 @@ BEGIN
         rst => general_rst
     );
 
-    CF_MAP: CF port map(
+    CF_MAP : CF PORT MAP(
         CF_Selected => CF_selected_SIG,
         DE_Carry_en_out => DE_Carry_en_out,
         MW_FLAGS_en_out => MW_FLAGS_en_out,
         clk => clk,
-        rst =>general_rst,
+        rst => general_rst,
         CF_out => CF_out
     );
-
-
-    MUX_ALU_OP1_MAP: MUX_ALU_OP1 port map (
+    MUX_ALU_OP1_MAP : MUX_ALU_OP1 PORT MAP(
         DE_Read_Data1_out => DE_Read_Data1_out,
         MM_ALU_Out_out => MM_ALU_Out,
         EM_ALU_out => EM_ALU_out,
@@ -277,7 +239,7 @@ BEGIN
         Read_data1_sel => Read_data1_sel_sig
     );
 
-    MUX_ALU_OP2_MAP: MUX_ALU_OP2 port map (
+    MUX_ALU_OP2_MAP : MUX_ALU_OP2 PORT MAP(
         DE_Read_Data2_out => DE_Read_Data2_out,
         MM_ALU_Out_out => MM_ALU_Out,
         EM_ALU_out => EM_ALU_out,
@@ -286,7 +248,7 @@ BEGIN
         Read_data2_sel => Read_data2_sel_sig
     );
 
-    Data_forwarding_MAP: Data_forwarding port map(
+    Data_forwarding_MAP : Data_forwarding PORT MAP(
         DE_Read_Address1 => DE_Read_Address1,
         DE_Read_Address2 => DE_Read_Address2,
         EM_Write_Addr_out => EM_Write_Addr_out,
@@ -299,7 +261,7 @@ BEGIN
         Read_data2_sel => Read_data2_sel_sig
     );
 
-    Structural_HDU_MAP: Structural_HDU port map(
+    Structural_HDU_MAP : Structural_HDU PORT MAP(
         DE_MemRead_en_out => DE_MemRead_en_out,
         EM_MemRead_en_out => EM_MemRead_en_out,
         DE_MemWrite_en_out => DE_MemWrite_en_out,
@@ -307,18 +269,18 @@ BEGIN
         en_structural => en_structural
     );
 
-    ALU_MAP: ALU port map(
+    ALU_MAP : ALU PORT MAP(
         Read_data1 => DE_Read_Data1_final_out_sig,
         Read_data2 => DE_Read_Data2_final_out_sig,
-        FUNC => DE_OPCODE_out (2 downto 0),
+        FUNC => DE_OPCODE_out (2 DOWNTO 0),
         ALU_en => DE_ALU_en_out,
         ALU_out => ALU_out,
         ZF_ALU => ZF_SIG,
         CF_ALU => CF_SIG,
         NF_ALU => NF_SIG
     );
-    
-    Stack_Pointer_MAP: Stack_Pointer port map(
+
+    Stack_Pointer_MAP : Stack_Pointer PORT MAP(
         clk => clk,
         rst => general_rst,
         en => DE_SP_en_out,
@@ -326,16 +288,12 @@ BEGIN
         out_data => SP_before_sig
     );
 
-    SP_ALU_MAP: SP_ALU port map (
+    SP_ALU_MAP : SP_ALU PORT MAP(
         SP_en => DE_SP_en_out,
         inc_en => DE_SP_inc_en_out,
         SP_after => SP_after_sig,
         SP_before => SP_before_sig
     );
-
-
-
-  
     --Remaining Output Signals
     DE_Read_Data1_final <= DE_Read_Data1_final_out_sig;
     DE_Read_Data2_final <= DE_Read_Data2_final_out_sig;
@@ -345,8 +303,4 @@ BEGIN
     SP_after <= SP_after_sig;
     SP_before <= SP_before_sig;
     DE_RTI_en_out <= MW_RTI_en_out;
-
-
-    
-
 END ARCHITECTURE;
